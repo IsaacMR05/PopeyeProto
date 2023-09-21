2e32a011ce6c92447715b64e705c73224b8053d3
Isaac <isaacmedrod@hotmail.com>
2023-09-21 13:04:54 +0200

    Started heavy attack

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -25,7 +25,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private BoxCollider attackCollider;
     [SerializeField] private float timeToShowAttackZone = 0.5f;
     private bool attacking = false;
+    private bool hasAnchor = true;
 
+    [Header("Heavy Attack")] 
+    [SerializeField] private GameObject heavyAttackRangeIndicator;
+    [SerializeField] private int heavyAttackDamage = 1;
+    [SerializeField] private float maxRange = 20.0f;
+    [SerializeField] private float timeToChargeAtMaxRange = 1.5f;
+    private bool chargingHeavyAttack = false;
+    private bool arrivedMaxRange = false;
+    private float timeCharging = 0.0f;
 
     public void Update()
     {
@@ -35,11 +44,38 @@ public class PlayerController : MonoBehaviour
         
         //Rotate Body to Look to the pointing axis
         playerObject.transform.LookAt(new Vector3(playerObject.transform.position.x + movementInput.x,playerObject.transform.position.y,playerObject.transform.position.z + movementInput.y));
+
+        if (Gamepad.current.leftTrigger.IsPressed() && !arrivedMaxRange)
+        {
+            //Heavy Attack
+            chargingHeavyAttack = true;
+            timeCharging += Time.deltaTime;
+            Debug.Log("Charging Heavy Attack");
+
+            
+            if (timeCharging >= timeToChargeAtMaxRange)
+            {
+                arrivedMaxRange = true;
+                timeCharging = 1.5f;
+            }
+        }
+        else if (chargingHeavyAttack)
+        {
+            Debug.Log("Throwing Anchor");
+            //Stop charging and throw heavy attack
+            movementForce = 1.5f;
+            hasAnchor = chargingHeavyAttack = false;
+            
+            //Throw the anchor to the looking vector of the player + reset timeCharging
+            float anchorPoint = (maxRange / timeToChargeAtMaxRange) * timeCharging;
+            timeCharging = 0.0f;
+            heavyAttackRangeIndicator.transform.localScale.Set(anchorPoint, 0.0f , 0.0f);
+        }
     }
 
     public void Attack()
     {
-        if (attacking) return;
+        if (attacking || !hasAnchor) return;
         Debug.Log("attack");
         StartCoroutine(ShowAttackZone());
     }
